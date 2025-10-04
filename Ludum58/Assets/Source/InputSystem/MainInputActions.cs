fa2c0cef9303b1bd084830ab62a849ca7c1baab5
arsmih24 <arsmih24@gmail.com>
2025-10-05 02:44:56 +0300

    feat: PlayerController

--- Ludum58/Assets/Source/InputSystem/MainInputActions.cs
+++ Ludum58/Assets/Source/InputSystem/MainInputActions.cs
@@ -99,7 +99,7 @@ public partial class @MainInputActions: IInputActionCollection2, IDisposable
                     ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
-                    ""initialStateCheck"": false
+                    ""initialStateCheck"": true
                 },
                 {
                     ""name"": ""Sprint"",
---
+++ Ludum58/Assets/Source/PlayerSystem/PlayerController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
